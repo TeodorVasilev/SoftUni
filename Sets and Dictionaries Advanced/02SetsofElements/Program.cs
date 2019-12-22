e9ca2c2b320779b68ade68eb8c34e9a5f21919c5
Teodor Vasilev <silver123@abv.bg>
2019-12-22 17:10:58 +0200

    Added solution for SetsOfElments exercise

--- Sets and Dictionaries Advanced/02SetsofElements/Program.cs
+++ Sets and Dictionaries Advanced/02SetsofElements/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace _02SetsofElements
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			var firstSetSize = input[0];
+			var secondSetSize = input[1];
+
+			var firstSet = new HashSet<int>();
+			var secondSet = new HashSet<int>();
+
+			for (int i = 0; i < firstSetSize; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+
+				firstSet.Add(number);
+			}
+			
+			for (int i = 0; i < secondSetSize; i++)
+			{
+				int number = int.Parse(Console.ReadLine());
+
+				secondSet.Add(number);
+			}
+
+			Console.WriteLine(string.Join(" ", firstSet.Intersect(secondSet)));
 		}
 	}
 }
