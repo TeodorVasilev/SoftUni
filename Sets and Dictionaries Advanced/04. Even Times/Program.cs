f3e8625c9a57752e3c734ed60e48513627537ece
Teodor Vasilev <silver123@abv.bg>
2019-12-22 17:33:38 +0200

    Added solution for EvenTimes exercice

---
+++ Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Even_Times
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+
+			var numbers = new Dictionary<int, int>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var number = int.Parse(Console.ReadLine());
+
+				if(numbers.ContainsKey(number))
+				{
+					numbers[number]++;
+				}
+				else
+				{
+					numbers[number] = 1;
+				}
+			}
+
+			foreach (var kvp in numbers)
+			{
+				if(kvp.Value % 2 == 0)
+				{
+					Console.WriteLine(kvp.Key);
+				}
+			}
+		}
+	}
+}
