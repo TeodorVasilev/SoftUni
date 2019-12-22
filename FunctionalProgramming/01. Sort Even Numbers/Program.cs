2788d914adfa217c960ceb5b5cedbe9c20d45984
Teodor Vasilev <silver123@abv.bg>
2019-12-22 15:50:58 +0200

    Added exercise for SortEvenNumbers

---
+++ FunctionalProgramming/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sort_Even_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(string.Join(", ", Console.ReadLine().Split(new char[] {',',' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(n => n % 2 == 0).OrderBy(n => n).ToList()));
+		}
+	}
+}
