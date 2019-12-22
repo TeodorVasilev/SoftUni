9eafcce6eb8b7ce686afc57edc0bd69edcf982ab
Teodor Vasilev <silver123@abv.bg>
2019-12-22 15:24:16 +0200

    Added solution for 01.UniqueUsernames

--- Sets and Dictionaries Advanced/01. Unique Usernames/Program.cs
+++ Sets and Dictionaries Advanced/01. Unique Usernames/Program.cs
@@ -10,6 +10,21 @@ namespace _01.Unique_Usernames
 	{
 		static void Main(string[] args)
 		{
+			int n = int.Parse(Console.ReadLine());
+
+			var names = new HashSet<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var name = Console.ReadLine();
+
+				names.Add(name);
+			}
+
+			foreach (var name in names)
+			{
+				Console.WriteLine(name);
+			}
 		}
 	}
 }
