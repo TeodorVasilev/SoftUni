4ab03a4e623f4b66665e765c40e9e9cd2497f5e4
Teodor Vasilev <silver123@abv.bg>
2019-12-22 17:18:48 +0200

    Added solution for PeriodicTable Exercice

---
+++ Sets and Dictionaries Advanced/03. Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Periodic_Table
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			var sortedElements = new SortedSet<string>();
+
+			for (int i = 0; i < n; i++)
+			{
+				var input = Console.ReadLine().Split().ToArray();
+
+				for (int k = 0; k < input.Length; k++)
+				{
+					sortedElements.Add(input[k]);
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", sortedElements));
+		}
+	}
+}
