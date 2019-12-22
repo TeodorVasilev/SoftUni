b32c1eaabf13f2ba90c7bd096d57240dfafa4f9c
Teodor Vasilev <silver123@abv.bg>
2019-12-22 17:40:26 +0200

    Added solution for CountSymbols Exercice

---
+++ Sets and Dictionaries Advanced/05. Count Symbols/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Count_Symbols
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine();
+
+			var symbolCount = new SortedDictionary<char, int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				var symbol = input[i];
+
+				if(symbolCount.ContainsKey(symbol))
+				{
+					symbolCount[symbol]++;
+				}
+				else
+				{
+					symbolCount[symbol] = 1;
+				}
+			}
+
+			foreach (var kvp in symbolCount)
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+			}
+		}
+	}
+}
