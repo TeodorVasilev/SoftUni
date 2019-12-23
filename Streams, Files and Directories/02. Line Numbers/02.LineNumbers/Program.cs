d9371cc5fb23c6626dde97ed7b54293426063909
Teodor Vasilev <silver123@abv.bg>
2019-12-23 15:09:52 +0200

    Added solution for 02.Line Numbers

---
+++ Streams, Files and Directories/02. Line Numbers/02.LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var reader = new StreamReader(@"..\..\..\Input1.txt"))
+			{
+				int counter = 1;
+
+				while (true)
+				{
+					var line = reader.ReadLine();
+
+					if(line == null)
+					{
+						break;
+					}
+
+					line = $"{counter}. {line}";
+					counter++;
+
+					using (var writer = new StreamWriter("Output.txt", true))
+					{
+						writer.WriteLine(line);
+					}
+				}
+			}
+		}
+	}
+}
