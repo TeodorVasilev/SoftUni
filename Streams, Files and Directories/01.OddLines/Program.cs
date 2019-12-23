75c998e4deeaa29a2e2711ddcdebed582cdb67f3
Teodor Vasilev <silver123@abv.bg>
2019-12-23 14:52:25 +0200

    Added solution for Streams, Files and Directories

---
+++ Streams, Files and Directories/01.OddLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var reader = new StreamReader(@"Input.txt"))
+			{
+				int counter = 0;
+
+				while (true)
+				{
+					var line = reader.ReadLine();
+
+					if (line == null)
+					{
+						break;
+					}
+
+					if(counter % 2 != 0)
+					{
+						Console.WriteLine(line);
+					}
+
+					counter++;
+				}
+			}
+		}
+	}
+}
