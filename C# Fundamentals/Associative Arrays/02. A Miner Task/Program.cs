33cc0d732abcafb0750ec3881972e402b24f5321
Teodor Vasilev <silver123@abv.bg>
2019-12-24 12:21:17 +0200

    Added solution 02. Miners Task exercice

---
+++ C# Fundamentals/Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinersTask
+{
+	public class MinerTask
+	{
+		public static void Main()
+		{
+			var resources = new Dictionary<string, int>();
+
+			while (true)
+			{
+				var name = Console.ReadLine();
+
+				if (name.ToLower() == "stop")
+				{
+					break;
+				}
+
+				var amount = int.Parse(Console.ReadLine());
+
+				if (!resources.ContainsKey(name))
+				{
+					resources[name] = 0;
+				}
+
+				resources[name] += amount;
+			}
+
+			Console.WriteLine(string.Join(Environment.NewLine, resources.Select(kvp => $"{kvp.Key} -> {kvp.Value}")));
+
+		}
+	}
+}
