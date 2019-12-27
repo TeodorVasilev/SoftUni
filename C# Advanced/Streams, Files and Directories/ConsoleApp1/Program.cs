5be5c927f2d2018e715c685cfb3bbb3d1fd2cd22
Teodor Vasilev <silver123@abv.bg>
2019-12-27 17:19:24 +0200

    Added solution 03WordCount

---
+++ C# Advanced/Streams, Files and Directories/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordsCount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var wordsReader = new StreamReader(@"words.txt"))
+			{
+				using (var textReader = new StreamReader(@"text.txt"))
+				{
+					using (var resultWriter = new StreamWriter(@"output.txt", true))
+					{
+						var wordsCount = new Dictionary<string, int>();
+						var line = wordsReader.ReadLine();
+						while (line != null)
+						{
+							var splitted = line.Split(' ');
+							for (int i = 0; i < splitted.Length; i++)
+							{
+								wordsCount.Add(splitted[i], 0);
+							}
+							line = wordsReader.ReadLine();
+						}
+
+						var textLine = textReader.ReadLine();
+						while (textLine != null)
+						{
+							var splitted = textLine.Split(new char[] { '-', '.', ',', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+							for (int i = 0; i < splitted.Length; i++)
+							{
+								if (wordsCount.ContainsKey(splitted[i].ToLower()))
+								{
+									wordsCount[splitted[i].ToLower()]++;
+								}
+							}
+
+							textLine = textReader.ReadLine();
+						}
+
+						var ordered = wordsCount.OrderByDescending(x => x.Value);
+
+						foreach (var kvp in ordered)
+						{
+							resultWriter.WriteLine($"{kvp.Key} - {kvp.Value}");
+						}
+					}
+				}
+			}
+		}
+	}
+}
