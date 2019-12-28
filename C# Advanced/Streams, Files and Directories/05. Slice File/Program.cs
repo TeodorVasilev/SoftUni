681bcd961bd7cf2454d9748786f0bdb8a086b2c3
Teodor Vasilev <silver123@abv.bg>
2019-12-28 14:43:30 +0200

    Added part of solution for 05. Slice File exercice

---
+++ C# Advanced/Streams, Files and Directories/05. Slice File/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace _05.Slice_File
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int fileCount = int.Parse(Console.ReadLine());
+
+			using (var reader = new FileStream(@"sliceme.txt", FileMode.Open))
+			{
+				var partLength = Math.Ceiling((double)reader.Length / fileCount);
+
+				for (int i = 1; i <= fileCount; i++)
+				{
+					var currentFileName = $"slice-{i}.txt";
+
+					var currentFileTotalBytes = 0;
+
+
+					using (var writer = new FileStream(currentFileName, FileMode.Create))
+					{
+
+						var buffer = new byte[4096];
+
+						while (true)
+						{
+							var totalRead = reader.Read(buffer, 0, buffer.Length);
+
+							if (totalRead == 0)
+							{
+								break;
+							}
+
+							currentFileTotalBytes += totalRead;
+
+							if (currentFileTotalBytes >= partLength)
+							{
+								break;
+							}
+
+							writer.Write(buffer, 0, buffer.Length);
+						}
+					}
+				}
+			}
+		}
+	}
+}
