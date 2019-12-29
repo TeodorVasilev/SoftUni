c206b1abf1949bdd09a15fed9ec417ca0329e2ab
Teodor Vasilev <silver123@abv.bg>
2019-12-29 16:44:14 +0200

    Added solution for 04. Add VAT exercice

---
+++ C# Advanced/FunctionalProgramming/04. Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Add_VAT
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<double> prices = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(double.Parse).Select(n => n * 1.2).ToList();
+			foreach (var price in prices)
+			{
+				Console.WriteLine($"{price:f2}");
+			}
+		}
+	}
+}
