fd682e99af490f81e4ddde040918e05293602583
Teodor Vasilev <silver123@abv.bg>
2019-12-29 16:24:50 +0200

    Added solution for 02.Sum Numbers exercice

---
+++ C# Advanced/FunctionalProgramming/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sum_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+			Console.WriteLine(numbers.Count);
+			Console.WriteLine(numbers.Sum());
+		} 
+	}
+}
