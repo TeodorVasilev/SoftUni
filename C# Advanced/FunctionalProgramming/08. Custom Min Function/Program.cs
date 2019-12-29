4e39d86ffced298fc839ec2519d55b8d9f573224
Teodor Vasilev <silver123@abv.bg>
2019-12-29 18:17:09 +0200

    Added solution 08 Custom Min Function

---
+++ C# Advanced/FunctionalProgramming/08. Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Custom_Min_Function
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			Console.WriteLine(numbers.Min());
+		}
+	}
+}
