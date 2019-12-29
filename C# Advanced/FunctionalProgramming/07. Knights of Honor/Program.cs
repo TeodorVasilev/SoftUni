3e2a6df0b70afc08a64830e8a0d1c58e04133227
Teodor Vasilev <silver123@abv.bg>
2019-12-29 18:04:06 +0200

    Added solution for 07.Knights of Honor exercice

---
+++ C# Advanced/FunctionalProgramming/07. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.Knights_of_Honor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> names = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+			names.ForEach(delegate (string name)
+			{
+				Console.WriteLine($"Sir {name}");
+			});
+		}
+	}
+}
