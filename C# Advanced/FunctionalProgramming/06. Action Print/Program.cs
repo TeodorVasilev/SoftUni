e03824c8f14cb5c96be3f4f841f9386479100726
Teodor Vasilev <silver123@abv.bg>
2019-12-29 17:59:28 +0200

    Added solution for 06. Action Print

---
+++ C# Advanced/FunctionalProgramming/06. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Action_Print
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> names = Console.ReadLine().Split(' ').ToList();
+
+			names.ForEach(delegate (string name)
+			{
+				Console.WriteLine(name);
+			});
+		}
+	}
+}
