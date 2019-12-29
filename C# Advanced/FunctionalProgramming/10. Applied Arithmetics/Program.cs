b7bafb2a86284faaf8f2cfb5b40ed54fe7ed3fe8
Teodor Vasilev <silver123@abv.bg>
2019-12-29 18:44:02 +0200

    Added solution for 10.Applied Arithmetics

---
+++ C# Advanced/FunctionalProgramming/10. Applied Arithmetics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.Applied_Arithmetics
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			string command = Console.ReadLine();
+
+			Action<string> manipulateList = operation =>
+			{
+				switch (operation)
+				{
+					case "add":
+						numbers = numbers.Select(n => n + 1).ToList();
+						break;
+					case "multiply":
+						numbers = numbers.Select(n => n * 2).ToList();
+						break;
+					case "subtract":
+						numbers = numbers.Select(n => n - 1).ToList();
+						break;
+					case "print":
+						Console.WriteLine(string.Join(" ", numbers));
+						break;
+				}
+			};
+
+			while (command != "end")
+			{
+				manipulateList(command);
+
+				command = Console.ReadLine();
+			}
+		}
+	}
+}
