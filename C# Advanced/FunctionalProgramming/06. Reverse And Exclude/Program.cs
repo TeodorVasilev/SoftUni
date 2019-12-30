4c895496615140c45ba6773b55f4d8464e165358
Teodor Vasilev <silver123@abv.bg>
2019-12-30 14:49:05 +0200

    Added solution for 11.Reverse and Exclude

---
+++ C# Advanced/FunctionalProgramming/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.Reverse_And_Exclude
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			int n = int.Parse(Console.ReadLine());
+
+			numbers.Reverse();
+
+			Func<int, bool> Filter = x => x % n != 0;
+
+			var remainingNumbers = numbers.Where(Filter).ToList();
+
+			Console.WriteLine(string.Join(" ", remainingNumbers));
+		}
+	}
+}
