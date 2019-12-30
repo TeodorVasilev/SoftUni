81d003a63c5dcb251c2882e227fe8f143ba3ad04
Teodor Vasilev <silver123@abv.bg>
2019-12-30 16:12:32 +0200

    Added Solution for 13. Custom Comparator

---
+++ C# Advanced/FunctionalProgramming/13. Custom Comparator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _13.Custom_Comparator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			Func<int, int, int> sortFunc = (a, b) => a.CompareTo(b);
+
+			Action<int[],int[]> print = (x,y) => Console.Write($"{string.Join(" ", x)} {string.Join(" ",y)}");
+
+			int[] evenNumbers = numbers.Where(x => x % 2 == 0).ToArray();
+			int[] oddNumbers = numbers.Where(x => x % 2 != 0).ToArray();
+
+			Array.Sort(evenNumbers, new Comparison<int>(sortFunc));
+			Array.Sort(oddNumbers, new Comparison<int>(sortFunc));
+			
+			print(evenNumbers, oddNumbers);
+		}
+	}
+}
