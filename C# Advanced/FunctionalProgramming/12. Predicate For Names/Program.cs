0fda54076f2695b38ae936569b3cef1934ac8442
Teodor Vasilev <silver123@abv.bg>
2019-12-30 15:51:48 +0200

    Added solution for 12. Predicate for Names exercice

---
+++ C# Advanced/FunctionalProgramming/12. Predicate For Names/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.Predicate_For_Names
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nameLenght = int.Parse(Console.ReadLine());
+
+			List<string> names = Console.ReadLine().Split().ToList();
+
+			Func<string, bool> filter = x => x.Length <= nameLenght;
+
+			var sorted = names.Where(filter).ToList();
+
+			foreach (var name in sorted)
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
+}
