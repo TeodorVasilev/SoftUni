77dfa4387fb829c0646dc02718294f8e92670b48
Teodor Vasilev <silver123@abv.bg>
2019-12-30 16:32:35 +0200

    Added solution for 14.List of Predicates

---
+++ C# Advanced/FunctionalProgramming/14. List Of Predicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _14.List_Of_Predicates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int endNumber = int.Parse(Console.ReadLine());
+
+			int[] dividers = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+
+			Queue<int> numbers = new Queue<int>();
+
+			var predicates = dividers.Select(div => (Func<int, bool>)(n => n % div == 0)).ToArray();
+
+			for (int i = 1; i <= endNumber; i++)
+			{
+				if (IsValid(predicates, i))
+				{
+					numbers.Enqueue(i);
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", numbers));
+		}
+
+		private static bool IsValid(Func<int, bool>[] predicates, int num)
+		{
+			foreach (var predicate in predicates)
+			{
+				if (!predicate(num))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
