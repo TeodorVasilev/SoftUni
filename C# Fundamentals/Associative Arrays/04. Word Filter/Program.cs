7a42ce5edad4f4fd28922480b1d28a6803b47cd7
Teodor Vasilev <silver123@abv.bg>
2019-12-31 15:16:04 +0200

    Added solution 04. Word Filter

---
+++ C# Fundamentals/Associative Arrays/04. Word Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Word_Filter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] words = Console.ReadLine().Split().Where(w => w.Length % 2 == 0).ToArray();
+
+			foreach (var word in words)
+			{
+				Console.WriteLine(word);
+			}
+		}
+	}
+}
