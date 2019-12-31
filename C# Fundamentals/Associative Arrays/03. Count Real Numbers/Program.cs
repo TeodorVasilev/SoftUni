618a244ed65abd26d483dd4e678290a6d0d74da5
Teodor Vasilev <silver123@abv.bg>
2019-12-31 15:12:35 +0200

    Added solution 03. Count Real Numbers

---
+++ C# Fundamentals/Associative Arrays/03. Count Real Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Count_Real_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+			var sorted = new SortedDictionary<double, int>();
+
+			foreach (var number in numbers)
+			{
+				if(!sorted.ContainsKey(number))
+				{
+					sorted[number] = 1;
+				}
+				else
+				{
+					sorted[number]++;
+				}
+			}
+
+			foreach (var kvp in sorted)
+			{
+				Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+			}
+		}
+	}
+}
