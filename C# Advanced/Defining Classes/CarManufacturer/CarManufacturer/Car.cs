4bf6092aec3a728b968f466098f21b8a28142d26
Teodor Vasilev <silver123@abv.bg>
2020-01-02 19:26:07 +0200

    Updated solution

--- C# Advanced/Defining Classes/CarManufacturer/CarManufacturer/Car.cs
+++ C# Advanced/Defining Classes/CarManufacturer/CarManufacturer/Car.cs
@@ -7,5 +7,33 @@
 		public string Model { get; set; }
 
 		public int Year { get; set; }
+
+		public double FuelQuantity { get; set; }
+
+		public double FuelConsumption { get; set; }
+
+		public void Drive(double distance)
+		{
+			double expenceFuel = FuelConsumption * distance / 100;
+
+			if (expenceFuel > FuelQuantity)
+			{
+				System.Console.WriteLine("Not enough fuel to perform this trip!");
+			}
+			else
+			{
+				FuelQuantity -= distance / 100 * FuelConsumption;
+			}
+		}
+
+		public string GetInformation()
+		{
+			var sb = new System.Text.StringBuilder();
+			sb.AppendLine($"Make: {this.Make}");
+			sb.AppendLine($"Model: {this.Model}");
+			sb.AppendLine($"Year: {this.Year}");
+			sb.Append($"Fuel: {this.FuelQuantity:F2}L");
+			return sb.ToString();
+		}
 	}
 }
--- C# Advanced/Defining Classes/CarManufacturer/CarManufacturer/Program.cs
+++ C# Advanced/Defining Classes/CarManufacturer/CarManufacturer/Program.cs
@@ -11,8 +11,13 @@ namespace CarManufacturer
 			car.Make = "VW";
 			car.Model = "MK3";
 			car.Year = 1992;
+			car.FuelQuantity = 200;
+			car.FuelConsumption = 200;
+			car.Drive(2000);
 
-			Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+			Console.WriteLine(car.GetInformation());
+
+			//Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
 		}
 	}
 }
