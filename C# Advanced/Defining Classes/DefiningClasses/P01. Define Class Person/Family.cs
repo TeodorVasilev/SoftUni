0ef9b71a97f1c69ad087941104b61137d501e9c4
Teodor Vasilev <silver123@abv.bg>
2020-01-03 19:01:43 +0200

    Added solution for 02.Problem  Creating Constructors

---
+++ C# Advanced/Defining Classes/DefiningClasses/P01. Define Class Person/Family.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01._Define_Class_Person
+{
+	public class Family
+	{
+		private List<Person> members;
+
+		public List<Person> Members { get; set; }
+
+		public void AddMember(Person member)
+		{
+			Members.Add(member);
+		}
+
+		public Person GetOldestMember()
+		{
+			Person oldest = Members.Max
+			
+		}
+	}
+}
---
+++ C# Advanced/Defining Classes/DefiningClasses/P01. Define Class Person/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01._Define_Class_Person
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
