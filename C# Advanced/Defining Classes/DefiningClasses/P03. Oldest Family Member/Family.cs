d64eb6228dd7c0cc6fb5506ce25a303f41e05760
TeodorVasilev <56998524+TeodorVasilev@users.noreply.github.com>
2020-01-04 11:55:03 +0200

    Extended solution for P03. Oldest Family Member

---
+++ C# Advanced/Defining Classes/DefiningClasses/P03. Oldest Family Member/Family.cs
@@ -0,0 +1,22 @@
+﻿using P01._Define_Class_Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Oldest_Family_Member
+{
+	public class Family
+	{
+		private List<Person> members = new List<Person>();
+		
+		public void AddMember(Person member)
+		{
+			this.members.Add(member);
+		}
+		
+		public Person GetOldestMember()
+		{
+			return this.members.OrderByDescending(x => x.Age).FirstOrDefault();
+		}
+	}
+}
---
+++ C# Advanced/Defining Classes/DefiningClasses/P03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using P03._Oldest_Family_Member;
+using System;
+using System.Collections.Generic;
+
+namespace P01._Define_Class_Person
+{
+	public class StartUp
+	{
+		public static void Main(string[] args)
+		{
+			int familyMembers = int.Parse(Console.ReadLine());
+
+			Family family = new Family();
+
+			for (int i = 0; i < familyMembers; i++)
+			{
+				string input = Console.ReadLine();
+
+				var inputParts = input.Split();
+				
+				string name = inputParts[0];
+				int age = int.Parse(inputParts[1]);
+
+				Person person = new Person(name, age);
+
+				family.AddMember(person);
+			}
+
+			Person oldestMember = family.GetOldestMember();
+
+			Console.WriteLine(oldestMember);
+		}	
+	} 
+}
