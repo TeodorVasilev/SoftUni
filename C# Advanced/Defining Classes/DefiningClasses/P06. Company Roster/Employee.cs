b99ec9eed508c758a60432da22dc10da9c6ef24c
Teodor Vasilev <silver123@abv.bg>
2020-01-04 14:47:45 +0200

    Updated solution for P05. Company Roster

---
+++ C# Advanced/Defining Classes/DefiningClasses/P06. Company Roster/Employee.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P06._Company_Roster
+{
+	public class Employee
+	{
+		//Mandatory:
+		//private string name;
+		//private int salary;
+		//private string position;
+		//private string department;
+		//Optional:
+		//private string email;
+		//private int age;
+
+		private string name;
+
+		private decimal salary;
+
+		private string position;
+
+		private string department;
+
+		private string email;
+
+		private int? age;
+
+		public Employee(string name, decimal salary, string position, string department)
+		{
+			this.Name = name;
+			this.Salary = salary;
+			this.Position = position;
+			this.Department = department;
+		}
+
+		public Employee(string name, decimal salary, string position, string department, string email)
+			:this(name, salary, position, department)
+		{
+			this.Email = email;
+		}
+
+		public Employee(string name, decimal salary, string position, string department, int? age)
+			:this(name, salary, position, department)
+		{
+			this.Age = age;
+		}
+
+		public Employee(string name, decimal salary, string position, string department, string email, int? age)
+			:this(name, salary, position, department)
+		{
+			this.Email = email;
+			this.Age = age;
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public decimal Salary
+		{
+			get { return salary; }
+			set { salary = value; }
+		}
+
+		public string Position
+		{
+			get { return position; }
+			set { position = value; }
+		}
+
+		public string Department
+		{
+			get { return department; }
+			set { department = value; }
+		}
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+		public int? Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append($"{this.Name} ");
+			sb.Append($"{this.Salary:f2} ");
+			sb.Append($"{(this.Email == null ? "n/a" : this.Email)} ");
+			sb.Append($"{(this.Age == null ? -1 : this.Age)}");
+
+			return sb.ToString(); 
+		}
+	}
+}
--- C# Advanced/Defining Classes/DefiningClasses/P06. Company Roster/Program.cs
+++ C# Advanced/Defining Classes/DefiningClasses/P06. Company Roster/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P06._Company_Roster
 {
@@ -6,6 +8,69 @@ namespace P06._Company_Roster
 	{
 		static void Main(string[] args)
 		{
+			int numberOfEmployees = int.Parse(Console.ReadLine());
+
+			List<Employee> employees = new List<Employee>();
+
+			for (int i = 0; i < numberOfEmployees; i++)
+			{
+				string[] employeeArgs = Console.ReadLine().Split().ToArray();
+				
+				string name = employeeArgs[0];
+				decimal salary = decimal.Parse(employeeArgs[1]);
+				string position = employeeArgs[2];
+				string department = employeeArgs[3];
+
+				if(employeeArgs.Length == 4)
+				{
+					Employee employee = new Employee(name, salary, position, department);
+
+					employees.Add(employee);
+				}
+				else if(employeeArgs.Length == 5)
+				{
+					if(employeeArgs[4].Contains("@"))
+					{
+						string email = employeeArgs[4];
+
+						Employee employee = new Employee(name, salary, position, department,email);
+
+						employees.Add(employee);
+					}
+					else
+					{
+						int age = int.Parse(employeeArgs[4]);
+
+						Employee employee = new Employee(name, salary, position, department, age);
+
+						employees.Add(employee);
+					}
+				}
+				else if(employeeArgs.Length == 6)
+				{
+					string email = employeeArgs[4];
+					int age = int.Parse(employeeArgs[5]);
+
+					Employee employee = new Employee(name, salary, position, department, email, age);
+
+					employees.Add(employee);
+				}
+			}
+
+			string bestDeptAverageSalary = employees
+				.GroupBy(e => e.Department)
+				.Select(g => new { Department = g.Key, AverageSalary = g.Average(e => e.Salary) })
+				.OrderByDescending(e => e.AverageSalary)
+				.First()
+				.Department;
+
+			Console.WriteLine($"Highest Average Salary: {bestDeptAverageSalary}");
+
+			employees
+				.Where(e => e.Department == bestDeptAverageSalary)
+				.OrderByDescending(e => e.Salary)
+				.ToList()
+				.ForEach(Console.WriteLine);
 		}
 	}
 }
