5382c67ac5c668c8c0e6c953c13fb3602444d289
Teodor Vasilev <silver123@abv.bg>
2020-01-04 17:34:35 +0200

    Updated solution for P07. Speed Racing

---
+++ C# Advanced/Defining Classes/DefiningClasses/P07. Speed Racing/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07._Speed_Racing
+{
+	public class Car
+	{
+		private string model;
+
+		private double fuelAmount;
+
+		private double fuelConsumptionForOneKm;
+
+		private double traveledDistance;
+
+		public Car(string model, double fuelAmount, double fuelConsumptionForOneKm, double traveledDistance)
+		{
+			this.Model = model;
+			this.FuelAmount = fuelAmount;
+			this.FuelConsumptionForOneKm = fuelConsumptionForOneKm;
+			this.TraveledDistance = TraveledDistance;
+		}
+
+		public double TraveledDistance
+		{
+			get { return traveledDistance; }
+			set { traveledDistance = value; }
+		}
+		
+		public double FuelConsumptionForOneKm
+		{
+			get { return fuelConsumptionForOneKm; }
+			set { fuelConsumptionForOneKm = value; }
+		}
+		
+		public double FuelAmount
+		{
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+		
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+		//disToTravel * fuelforonekm - fuelamount
+		public void Drive(double distance)
+		{
+			double expenceFuel = distance * FuelConsumptionForOneKm;
+
+			if (expenceFuel > FuelAmount)
+			{
+				System.Console.WriteLine("Insufficient fuel for the drive");
+			}
+			else
+			{
+				FuelAmount -= distance * FuelConsumptionForOneKm;
+				TraveledDistance += distance;
+			}
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append($"{this.Model} {this.FuelAmount:f2} {this.TraveledDistance}");
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ C# Advanced/Defining Classes/DefiningClasses/P07. Speed Racing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07._Speed_Racing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numberOfCars = int.Parse(Console.ReadLine());
+
+			List<Car> cars = new List<Car>();
+
+			for (int i = 0; i < numberOfCars; i++)
+			{
+				string[] carArgs = Console.ReadLine().Split().ToArray();
+
+				string model = carArgs[0];
+				double fuelAmount = double.Parse(carArgs[1]);
+				double fuelConsumptionForOneKm = double.Parse(carArgs[2]);
+
+				Car car = new Car(model, fuelAmount, fuelConsumptionForOneKm, 0);
+
+				cars.Add(car);
+			}
+
+			string input = Console.ReadLine();
+
+			while (input != "End")
+			{
+				string[] inputParts = input.Split();
+
+				string model = inputParts[1];
+				double distance = double.Parse(inputParts[2]);
+
+				Car car = cars.Single(c => c.Model == model);
+
+				car.Drive(distance);
+
+				input = Console.ReadLine();
+			}
+
+			cars.ForEach(Console.WriteLine);
+		}
+	}
+}
