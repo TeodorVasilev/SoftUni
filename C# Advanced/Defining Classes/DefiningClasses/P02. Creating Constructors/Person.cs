81eefde9633aa0a358d72932906c018c810f7435
TeodorVasilev <56998524+TeodorVasilev@users.noreply.github.com>
2020-01-04 10:07:33 +0200

    Add files via upload

---
+++ C# Advanced/Defining Classes/DefiningClasses/P02. Creating Constructors/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01._Define_Class_Person
+{
+	public class Person
+	{
+		private string name;
+
+		private int age;
+		
+		public Person(string name, int age)
+		{
+			this.Name = name;
+			this.Age = age;
+		}
+
+		public Person(int age):this("No name", age)
+		{
+		}
+
+		public Person():this("No name",1)
+		{
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		
+		public int Age 
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+	}
+}
