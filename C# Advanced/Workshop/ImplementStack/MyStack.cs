b9be4bc6f30895c69e34931916e3f797f8f0e1d0
Teodor Vasilev <silver123@abv.bg>
2020-01-06 15:35:58 +0200

    Added solution for Workshop P01. ImplementStack

---
+++ C# Advanced/Workshop/ImplementStack/MyStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImplementStack
+{
+	public class MyStack<T>
+	{
+		private T[] values;
+
+		private int count;
+
+		public MyStack()
+			: this(16)
+		{
+		}
+
+		public MyStack(int initialCapacity)
+		{
+			this.count = 0;
+			this.values = new T[initialCapacity];
+		}
+
+		public int Count
+		{
+			get
+			{
+				return this.count;
+			}
+		}
+
+		public void Push(T value)
+		{
+			if(this.count == this.values.Length)
+			{
+				var nextValues = new T[this.values.Length * 2];
+				for (int i = 0; i < this.values.Length; i++)
+				{
+					nextValues[i] = this.values[i];
+				}
+
+				this.values = nextValues;
+			}
+
+			this.values[this.count] = value;
+			this.count++;
+		}
+
+		public object Pop()
+		{
+			if(this.count == 0)
+			{
+				throw new InvalidOperationException("MyStack is empty.");
+			}
+
+			var lastIndex = this.count - 1;
+			var last = this.values[lastIndex];
+			this.count--;
+			return last;
+		}
+
+		public void ForEach(Action<object> action)
+		{
+			for (int i = 0; i < this.count; i++)
+			{
+				action(this.values[i]);
+			}
+		}
+	}
+}
---
+++ C# Advanced/Workshop/ImplementStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ImplementStack
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var stack = new MyStack<int>();
+
+			stack.Push(2);
+			stack.Push(5);
+			stack.Push(10);
+
+			Console.WriteLine(stack.Count == 3);
+
+			var value = (int)stack.Pop();
+
+			Console.WriteLine(value == 10);
+
+			Console.WriteLine(stack.Count == 2);
+
+			stack.ForEach(obj => Console.WriteLine(obj));
+		}
+	}
+}
