f519e8390fea7b8d41c2afc00d686de004e05290
Teodor Vasilev <silver123@abv.bg>
2020-01-06 17:49:28 +0200

    Added solution for Implementig Linked List

--- C# Advanced/Workshop/ImplementLinkedList/MyLinkedList.cs
+++ C# Advanced/Workshop/ImplementLinkedList/MyLinkedList.cs
@@ -4,16 +4,36 @@ namespace ImplementLinkedList
 {
 	public class MyLinkedList
 	{
-		private MyNode head;
-		private MyNode tail;
+		private MyNode head;// { get; private set; }
+		private MyNode tail;// { get; private set; }
 
 		public int Count { get; private set; }
+
+		public object Head
+		{
+			get
+			{
+				this.ValidateIfListIsEmpty();
+
+				return this.head.Value;
+			}
+		}
+
+		public object Tail
+		{
+			get
+			{
+				this.ValidateIfListIsEmpty();
+
+				return this.tail.Value;
+			}
+		}
 
 		public void AddHead(object value)
 		{
 			var newNode = new MyNode(value);
 
-			if(this.Count == 0)
+			if (this.Count == 0)
 			{
 				this.head = this.tail = newNode;
 			}
@@ -26,6 +46,174 @@ namespace ImplementLinkedList
 			}
 
 			this.Count++;
+		}
+
+		public void AddTail(object value)
+		{
+			var newNode = new MyNode(value);
+
+			if (this.Count == 0)
+			{
+				this.head = this.tail = newNode;
+			}
+			else
+			{
+				var oldTail = this.tail;
+				oldTail.Next = newNode;
+				newNode.Previous = oldTail;
+				this.tail = newNode;
+			}
+
+			this.Count++;
+		}
+
+		public object RemoveHead()
+		{
+			this.ValidateIfListIsEmpty();
+
+			var value = this.head.Value;
+
+			if (this.head == this.tail)
+			{
+				this.head = null;
+				this.tail = null;
+			}
+			else
+			{
+				var newHead = this.head.Next;
+				newHead.Previous = null;
+				this.head.Next = null;
+				this.head = newHead;
+			}
+
+			this.Count--;
+
+			return value;
+		}
+
+		public bool Contains(object value)
+		{
+			var currentNode = this.head;
+
+			while (currentNode != null)
+			{
+				if(currentNode.Value.Equals(value))
+				{
+					return true;
+				}
+
+				currentNode = currentNode.Next;
+			}
+
+			return false;
+		}
+
+		public object RemoveTail()
+		{
+			this.ValidateIfListIsEmpty();
+
+			var value = this.tail.Value;
+
+			if (this.tail == this.head)
+			{
+				this.tail = null;
+				this.head = null;
+			}
+			else
+			{
+				var newTail = this.tail.Previous;
+				newTail.Next = null;
+				this.tail.Previous = null;
+				this.tail = newTail;
+			}
+
+			this.Count--;
+
+			return value;
+		}
+
+		public void Remove(object value)
+		{
+			var currentNode = this.head;
+
+			while (currentNode != null)
+			{
+				var nodeValue = currentNode.Value;
+
+				if(nodeValue.Equals(value))
+				{
+					this.Count--;
+
+					var prevNode = currentNode.Previous;
+					var nextNode = currentNode.Next;
+
+					if(prevNode != null)
+					{
+						prevNode.Next = nextNode;
+					}
+
+					if(nextNode != null)
+					{
+						nextNode.Previous = prevNode;
+					}
+
+					if(this.head == currentNode)
+					{
+						this.head = nextNode;
+					}
+
+					if(this.tail == currentNode)
+					{
+						this.tail = prevNode;
+					}
+				}
+
+				currentNode = currentNode.Next;
+			}
+		}
+
+		public void Clear()
+		{
+			this.head = null;
+			this.tail = null;
+			this.Count = 0;
+		}
+
+		public void ForEach(Action<object> action, bool reverse = false)
+		{
+			var currentNode = reverse ? this.tail : this.head;
+
+			while (currentNode != null)
+			{
+				action(currentNode.Value);
+				currentNode = reverse ? currentNode.Previous : currentNode.Next;
+			}
+		}
+
+		public object[] ToArray()
+		{
+			var arr = new object[this.Count];
+
+			var currentNode = this.head;
+
+			var index = 0;
+
+			while (currentNode != null)
+			{
+				arr[index] = currentNode.Value;
+				index++;
+				currentNode = currentNode.Next;
+			}
+
+			return arr;
+		}
+
+		private void ValidateIfListIsEmpty()
+		{
+			if (this.Count == 0)
+			{
+				throw new InvalidOperationException("MyLinkedList is empty");
+			}
 		}
 	}
 }
--- C# Advanced/Workshop/ImplementLinkedList/Program.cs
+++ C# Advanced/Workshop/ImplementLinkedList/Program.cs
@@ -14,7 +14,63 @@ namespace ImplementLinkedList
 
 			//15 <-> 10 <-> 5
 
+			Console.WriteLine((int)linkedList.Head == 15);
+			Console.WriteLine((int)linkedList.Tail == 5);
 			Console.WriteLine(linkedList.Count == 3);
+
+			linkedList.AddTail(20);
+			linkedList.AddTail(25);
+
+			//15 <-> 10 <-> 5 <-> 20 <-> 25
+
+			//linkedList.ForEach(Console.WriteLine);
+			//Console.WriteLine();
+			//linkedList.ForEach(Console.WriteLine, true);
+			//Console.WriteLine();
+			//var arr = linkedList.ToArray();
+
+			//foreach (var item in arr)
+			//{
+			//	Console.WriteLine(item);
+			//}
+			
+			Console.WriteLine((int)linkedList.Head == 15);
+			Console.WriteLine((int)linkedList.Tail == 25);
+			Console.WriteLine(linkedList.Count == 5);
+
+			//Console.WriteLine((int)linkedList.RemoveHead() == 15);
+			//Console.WriteLine((int)linkedList.RemoveHead() == 10);
+			//Console.WriteLine((int)linkedList.Head == 5);
+			//Console.WriteLine(linkedList.Count == 3);
+
+			Console.WriteLine((int)linkedList.RemoveTail() == 25);
+			Console.WriteLine((int)linkedList.RemoveTail() == 20);
+			Console.WriteLine((int)linkedList.Tail == 5);
+			Console.WriteLine(linkedList.Count == 3);
+
+			linkedList = new MyLinkedList();
+
+			linkedList.AddTail(5);
+			linkedList.AddTail(10);
+			linkedList.AddTail(5);
+			linkedList.AddTail(20);
+			linkedList.AddTail(5);
+
+			linkedList.Remove(5);
+
+			Console.WriteLine((int)linkedList.Head == 10);
+			Console.WriteLine((int)linkedList.Tail == 20);
+			Console.WriteLine(linkedList.Count == 2);
+
+			Console.WriteLine(linkedList.Contains(10));
+			Console.WriteLine(linkedList.Contains(20));
+			Console.WriteLine(linkedList.Contains(5) == false);
+
+			linkedList.Clear();
+			
+			Console.WriteLine(linkedList.Count == 0);
+			Console.WriteLine(linkedList.Contains(10) == false);
+			Console.WriteLine(linkedList.Contains(20) == false);
 		}
 	}
 }
