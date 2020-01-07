80b20ac2ce8ebd160c402b37c32cbb9544364ad6
Teodor Vasilev <silver123@abv.bg>
2020-01-07 19:40:56 +0200

    Added solution for 03. Generic Equality Scale

---
+++ C# Advanced/Generics/Generic Scale/EqualityScale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Scale
+{
+	public class EqualityScale<T> where T : struct
+	{
+		private T left;
+
+		private T right;
+
+		public EqualityScale(T left, T right)
+		{
+			this.left = left;
+			this.right = right;
+		}
+		
+		public bool AreEqual()
+		{
+			if(this.left.Equals(this.right))
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ C# Advanced/Generics/Generic Scale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generic_Scale
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
