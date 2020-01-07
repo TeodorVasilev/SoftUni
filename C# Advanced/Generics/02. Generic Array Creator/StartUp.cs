4290ff192489b34035711a4e383b9d9c06dc726d
Teodor Vasilev <silver123@abv.bg>
2020-01-07 18:42:28 +0200

    Add solution for 02. Generic Array Creator

---
+++ C# Advanced/Generics/02. Generic Array Creator/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Generic_Array_Creator
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			string[] strings = ArrayCreator.Create(5, "Pesho");
+
+			int[] integers = ArrayCreator.Create(10, 20);
+
+			foreach (var item in strings)
+			{
+				Console.WriteLine(item == "Pesho");
+			}
+		}
+	}
+}
