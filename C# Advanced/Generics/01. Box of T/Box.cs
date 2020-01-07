d8516f7a46a06d2dc507d814a7181a870f326532
Teodor Vasilev <silver123@abv.bg>
2020-01-07 17:25:35 +0200

    Added solution for 01.Box of T exercise

---
+++ C# Advanced/Generics/01. Box of T/Box.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Box_of_T
+{
+	public class Box<T>
+	{
+		private T[] elements;
+
+		private int count;
+
+		public Box()
+			: this(16)
+		{
+
+		}
+
+		public Box(int initialCapacity)
+		{
+			this.count = 0;
+			this.elements = new T[initialCapacity];
+		}
+
+		public int Count
+		{
+			get
+			{
+				return this.count;
+			}
+		}
+
+		public void Add(T element)
+		{
+			if (this.count == this.elements.Length)
+			{
+				var nextValues = new T[this.elements.Length * 2];
+				for (int i = 0; i < this.elements.Length; i++)
+				{
+					nextValues[i] = this.elements[i];
+				}
+
+				this.elements = nextValues;
+			}
+
+			this.elements[this.count] = element;
+			this.count++;
+		}
+
+		public T Remove()
+		{
+
+			if (this.count == 0)
+			{
+				throw new InvalidOperationException("Box is empty.");
+			}
+
+			var lastIndex = this.count - 1;
+			var topMostElement = this.elements[lastIndex];
+			this.count--;
+			return topMostElement;
+		}
+	}
+}
---
+++ C# Advanced/Generics/01. Box of T/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Box_of_T
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			Box<int> box = new Box<int>();
+			box.Add(1);
+			box.Add(2);
+			box.Add(3);
+			Console.WriteLine(box.Remove() == 3);
+			box.Add(4);
+			box.Add(5);
+			Console.WriteLine(box.Remove() == 5);
+		}
+	}
+}
