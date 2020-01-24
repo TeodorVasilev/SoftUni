03c5d4639eae6e432b606a7690caa2bab593f805
Teodor Vasilev <silver123@abv.bg>
2020-01-24 10:32:41 +0200

    Added solutions from Polymorphism Lab

---
+++ C# OOP/Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿namespace Shapes
+{
+	using System;
+
+	public class Rectangle : Shape
+	{
+		private double height;
+		private double width;
+
+		public Rectangle(double height, double width)
+		{
+			this.Height = height;
+			this.Width = width;
+		}
+
+		public double Height 
+		{
+			get => this.height;
+
+			private set
+			{
+				if(value <= 0)
+				{
+					throw new ArgumentException("Height cannot be zero or negative.");
+				}
+
+				this.height = value;
+			}
+		}
+		public double Width
+		{
+			get => this.width;
+
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Width cannot be zero or negative.");
+				}
+
+				this.width = value;
+			}
+		}
+
+		public override double CalculateArea()
+		{
+			return this.Width * this.Height;
+		}
+
+		public override double CalculatePerimeter()
+		{
+			return 2 * (this.Width + this.Height);
+		}
+
+		public override string Draw()
+		{
+			return base.Draw() + this.GetType().Name; 
+		}
+	}
+}
