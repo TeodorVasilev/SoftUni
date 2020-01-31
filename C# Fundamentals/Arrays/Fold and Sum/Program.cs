378d464852b597123268755b412858c57ec18428
Teodor Vasilev <silver123@abv.bg>
2020-01-31 13:15:11 +0200

    Added homework for Arrays

---
+++ C# Fundamentals/Arrays/Fold and Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Fold_and_Sum
+{
+	using System;
+    using System.Linq;
+
+    public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			int k = numbers.Length / 4;
+
+			int[] left = numbers.Take(k).Reverse().ToArray();
+			int[] right = numbers.Reverse().Take(k).ToArray();
+
+			int[] firstRow = left.Concat(right).ToArray();
+			int[] secondRow = numbers.Skip(k).Take(k * 2).ToArray();
+
+			int[] sum = firstRow.Select((x, index) => x + secondRow[index]).ToArray();
+
+			Console.WriteLine(string.Join(" ", sum));
+		}
+	}
+}
---
+++ C# Fundamentals/Arrays/Recursive Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Recursive_Fibonacci
+{
+	using System;
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			Console.Write(Fibonacci(n - 1) + " ");
+		}
+		public static int Fibonacci(int n)
+		{
+			if (n <= 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return Fibonacci(n - 2) + Fibonacci(n - 1);
+			}
+		}
+	}
+}
