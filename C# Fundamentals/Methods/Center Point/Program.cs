181b03db50b4b95c67b6ae3492837cd3bfd61ead
Teodor Vasilev <silver123@abv.bg>
2020-02-07 11:46:13 +0200

    Added homework for Methods

---
+++ C# Fundamentals/Methods/Center Point/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Center_Point
+{
+	using System;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double x1 = double.Parse(Console.ReadLine());
+			double y1 = double.Parse(Console.ReadLine());
+			double x2 = double.Parse(Console.ReadLine());
+			double y2 = double.Parse(Console.ReadLine());
+
+			PrintClosePointToCenter(x1, y1, x2, y2);
+		}
+
+		static void PrintClosePointToCenter(double x1, double y1, double x2, double y2)
+		{
+			double p1 = Math.Abs(Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2)));
+			double p2 = Math.Abs(Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2)));
+
+			if (p2 < p1)
+			{
+				Console.WriteLine($"({x2}, {y2})");
+			}
+			else
+			{
+				Console.WriteLine($"({x1}, {y1})");
+			}
+		}
+	}
+}
---
+++ C# Fundamentals/Methods/Data Types/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Data_Types
+{
+	using System;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputType = Console.ReadLine();
+
+			if(inputType == "int")
+			{
+				int a = int.Parse(Console.ReadLine());
+				PrintInput(a);
+			}
+			else if(inputType == "real")
+			{
+				double a = double.Parse(Console.ReadLine());
+				PrintInput(a);
+			}
+			else if(inputType =="string")
+			{
+				string a = Console.ReadLine();
+				PrintInput(a);
+			}
+		}
+
+		static void PrintInput(int a)
+		{
+			Console.WriteLine(a * 2);
+		}
+
+		static void PrintInput(double a)
+		{
+			Console.WriteLine($"{a * 1.5:f2}");
+		}
+
+		static void PrintInput(string a)
+		{
+			Console.WriteLine($"${a}$");
+		}
+	}
+}
