ba41cdae60096133ee515adc802f3f36984aa273
Teodor Vasilev <silver123@abv.bg>
2020-02-07 22:08:46 +0200

    Added solution for first exercise from Reflection and Attributes

---
+++ C# OOP/Reflection and Attributes/Stealer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+	using System;
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var spy = new Spy();
+			var result = spy.StealFieldInfo("Hacker", "username", "password");
+			Console.WriteLine(result);
+		}
+	}
+}
---
+++ C# OOP/Reflection and Attributes/Stealer/Spy.cs
@@ -0,0 +1,19 @@
+﻿namespace Stealer
+{
+    using System.Reflection;
+    using System.Text;
+
+	public class Spy
+	{
+		public string StealFieldInfo(string name, params string[] fieldsNames)
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendLine($"Class under investigation: {nameof(Hacker)}");
+
+			var fields = typeof(Hacker).GetFields(BindingFlags.NonPublic);
+
+			return sb.ToString();
+		}
+	}
+}
