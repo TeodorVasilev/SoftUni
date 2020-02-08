ca2933dee340ce0ab76c2e9e3574820f32fda124
Teodor Vasilev <silver123@abv.bg>
2020-02-08 13:05:00 +0200

    Added solution for Stealer exercise from Reflection and Attributes

--- C# OOP/Reflection and Attributes/Stealer/Spy.cs
+++ C# OOP/Reflection and Attributes/Stealer/Spy.cs
@@ -1,19 +1,28 @@
 ﻿namespace Stealer
 {
+    using System;
+    using System.Linq;
     using System.Reflection;
     using System.Text;
 
 	public class Spy
 	{
-		public string StealFieldInfo(string name, params string[] fieldsNames)
-		{
-			var sb = new StringBuilder();
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type type = Type.GetType(className);
+            var instance = Activator.CreateInstance(type, new object[0]);
 
-			sb.AppendLine($"Class under investigation: {nameof(Hacker)}");
+            sb.AppendLine($"Class under investigation: {className}");
+            FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Static |
+                                                BindingFlags.Public | BindingFlags.NonPublic);
 
-			var fields = typeof(Hacker).GetFields(BindingFlags.NonPublic);
+            foreach (var field in fields.Where(field => fieldNames.Contains(field.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
 
-			return sb.ToString();
-		}
-	}
+            return sb.ToString().Trim();
+        }
+    }
 }
