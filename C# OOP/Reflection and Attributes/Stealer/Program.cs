cea2ba78aedde192b0484e8deb88fc89951fe28d
Teodor Vasilev <silver123@abv.bg>
2020-02-09 13:54:46 +0200

    Added solution for Stealer exersice from Reflection and Attributes Lab 01.Stealer, 02.AnalyzeAccesModifiers, 03.RevealPrivateMethods 04.CollectGettersAndSetters

--- C# OOP/Reflection and Attributes/Stealer/Program.cs
+++ C# OOP/Reflection and Attributes/Stealer/Program.cs
@@ -7,8 +7,17 @@
 		static void Main(string[] args)
 		{
 			var spy = new Spy();
-			var result = spy.StealFieldInfo("Hacker", "username", "password");
+			var result = spy.StealFieldInfo(typeof(Hacker).FullName, "username", "password");
 			Console.WriteLine(result);
+
+			var acessModifiersResult = spy.AnalyzeAcessModifiers(typeof(Hacker).FullName);
+			Console.WriteLine(acessModifiersResult);
+
+			var privateMethodsResult = spy.RevealPrivateMethods(typeof(Hacker).FullName);
+			Console.WriteLine(privateMethodsResult);
+
+			var collectResult = spy.CollectGettersAndSetters(typeof(Hacker).FullName);
+			Console.WriteLine(collectResult);
 		}
 	}
 }
--- C# OOP/Reflection and Attributes/Stealer/Spy.cs
+++ C# OOP/Reflection and Attributes/Stealer/Spy.cs
@@ -7,19 +7,86 @@
 
 	public class Spy
 	{
-        public string StealFieldInfo(string className, params string[] fieldNames)
+        public string StealFieldInfo(string className, params string[] fieldsNames)
         {
-            StringBuilder sb = new StringBuilder();
-            Type type = Type.GetType(className);
-            var instance = Activator.CreateInstance(type, new object[0]);
+            var sb = new StringBuilder();
+            var targetType = Type.GetType(className);
 
-            sb.AppendLine($"Class under investigation: {className}");
-            FieldInfo[] fields = type.GetFields(BindingFlags.Instance | BindingFlags.Static |
-                                                BindingFlags.Public | BindingFlags.NonPublic);
+            sb.AppendLine($"Class under investigation: {targetType}");
 
-            foreach (var field in fields.Where(field => fieldNames.Contains(field.Name)))
+            var classInstance = Activator.CreateInstance(targetType);
+
+            var fields = targetType.GetFields(BindingFlags.Instance | BindingFlags.Static |
+                BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var field in fields.Where(f => fieldsNames.Contains(f.Name)))
             {
-                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string AnalyzeAcessModifiers(string className)
+        {
+            var classType = Type.GetType(className);
+
+            var fieldInfo = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            var classPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            var classPrivateMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var sb = new StringBuilder();
+
+            foreach (var field in fieldInfo)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (var method in classPrivateMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            foreach (var method in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            var classType = Type.GetType(className);
+
+            var methodInfo = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"All private methods of Class: {className}");
+            sb.AppendLine($"Base class: {classType.BaseType.Name}");
+
+            foreach (var method in methodInfo)
+            {
+                sb.AppendLine($"{method.Name}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            var classType = Type.GetType(className);
+
+            var methodInfo = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+            var sb = new StringBuilder();
+
+            foreach (var method in methodInfo.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (var method in methodInfo.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
             }
 
             return sb.ToString().Trim();
