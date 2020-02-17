c799078c6511334c0f188f634aeaf438a01b7663
Teodor Vasilev <silver123@abv.bg>
2020-02-17 17:28:11 +0200

    Added exercises from Lists homework

---
+++ C# Fundamentals/Lists/Car Race/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Car_Race
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double leftTime = 0;
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+                else
+                {
+                    leftTime += numbers[i];
+                }
+            }
+
+            double rightTime = 0;
+
+            for (int i = numbers.Count - 1; i > numbers.Count / 2; i--)
+            {
+                if (numbers[i] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+                else
+                {
+                    rightTime += numbers[i];
+                }
+            }
+
+            if (leftTime < rightTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Lists/TakeSkip Rope/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace TakeSkip_Rope
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            char[] textSplit = text.ToCharArray();
+
+            List<int> numbers = new List<int>();
+            List<char> letters = new List<char>();
+
+            for (int i = 0; i < textSplit.Length; i++)
+            {
+                char symbol = textSplit[i];
+
+                if(char.IsDigit(symbol))
+                {
+                    int number = int.Parse(new string(symbol, 1));
+                    numbers.Add(number);
+                }
+
+                if (char.IsLetter(symbol) || char.IsPunctuation(symbol))
+                {
+                    letters.Add(symbol);
+                }
+            }
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    takeList.Add(numbers[i]);
+                }
+                else
+                {
+                    skipList.Add(numbers[i]);
+                }
+            }
+
+            char[] lettersToCharArr = letters.ToArray();
+
+            string lettersToString = new string(lettersToCharArr);
+
+            //string result = string.Empty;
+        }
+    }
+}
