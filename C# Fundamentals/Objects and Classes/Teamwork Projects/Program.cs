ceb095e57555ee71731020cdd2c78a8e4042effa
Teodor Vasilev <silver123@abv.bg>
2020-02-24 12:43:29 +0200

    Added homework for Objects and Classes

--- C# Fundamentals/Objects and Classes/Teamwork Projects/Program.cs
+++ C# Fundamentals/Objects and Classes/Teamwork Projects/Program.cs
@@ -1,12 +1,73 @@
 ﻿namespace Teamwork_Projects
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Team
+    {
+        public Team(string creatorName, string name)
+        {
+            this.Name = name;
+            this.Creator = creatorName;
+            this.Members = new List<string>();
+        }
+
+        public string Name { get; set; }
+
+        public string Creator { get; set; }
+
+        public List<string> Members { get; set; }
+
+        public void AddMember()
+        {
+
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            int teamsToRegister = int.Parse(Console.ReadLine());
 
+            List<Team> teams = new List<Team>();
+
+            for (int i = 0; i < teamsToRegister; i++)
+            {
+                string[] creatorTeam = Console.ReadLine().Split('-');
+
+                string creatorName = creatorTeam[0];
+                string teamName = creatorTeam[1];
+
+                Team team = new Team(creatorName, teamName);
+
+                if(!teams.Select(t => t.Name).Contains(team.Name))
+                {
+                    if(!teams.Select(t => t.Creator).Contains(team.Creator))
+                    {
+                        teams.Add(team);
+
+                        Console.WriteLine($"Team {team.Name} has been created by {team.Creator}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{team.Creator} cannot create another team!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Team {team.Name} was already created!");
+                }
+
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "end of assignment")
+            {
+
+            }
         }
     }
 }
