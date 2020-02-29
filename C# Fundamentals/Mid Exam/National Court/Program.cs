800e9f40838468936c96250cf89f435377e050cf
Teodor Vasilev <ksilometazolin@abv.bg>
2020-02-29 19:32:54 +0200

    Added solutions from C# Fundamentals Mid Exam

---
+++ C# Fundamentals/Mid Exam/National Court/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDeskPeople = int.Parse(Console.ReadLine());
+            int secondDeskPeople = int.Parse(Console.ReadLine());
+            int thirdDeskPeople = int.Parse(Console.ReadLine());
+            int totalPeopleCount = int.Parse(Console.ReadLine());
+
+            int peoplePerHour = firstDeskPeople + secondDeskPeople + thirdDeskPeople;
+            int hours = 0;
+
+            int serviced = 0;
+
+            while (totalPeopleCount > 0)
+            {
+                serviced += peoplePerHour;
+                totalPeopleCount -= peoplePerHour;
+                hours++;
+
+                if(hours % 4 == 0)
+                {
+                    hours++;
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
---
+++ C# Fundamentals/Mid Exam/Shoping List/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shoping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine().Split('!').ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Go Shopping!")
+            {
+                string[] commandArgs = input.Split();
+
+                string command = commandArgs[0];
+
+                if (command == "Unnecessary")
+                {
+                    string item = commandArgs[1];
+
+                    if (groceries.Contains(item))
+                        groceries.Remove(item);
+                }
+                else if (command == "Urgent")
+                {
+                    string item = commandArgs[1];
+
+                    if (!groceries.Contains(item))
+                        groceries.Insert(0, item);
+                }
+                else if (command == "Rearrange")
+                {
+                    string item = commandArgs[1];
+
+                    if(groceries.Contains(item))
+                    {
+                        int index = groceries.IndexOf(item);
+
+                        groceries.RemoveAt(index);
+                        groceries.Add(item);
+                    }
+                }
+                else if (command == "Correct")
+                {
+                    string oldItem = commandArgs[1];
+                    string newItem = commandArgs[2];
+
+                    if (groceries.Contains(oldItem))
+                    {
+                        int index = groceries.IndexOf(oldItem);
+                        groceries[index] = newItem;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", groceries));
+        }
+    }
+}
