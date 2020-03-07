5d39a34231937fb3ce5fc05205b50e958b344bfb
Teodor Vasilev <silver123@abv.bg>
2020-03-07 11:06:02 +0200

    Added exercise for Final Exam

---
+++ C# Fundamentals/Final Exam Exercises/Boss Rush/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Boss_Rush
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Regex rgx = new Regex("[|][A-Z]{4,}[|]:[#][A-Z][a-z]+ [a-z]+#");
+
+                bool isMatch = rgx.IsMatch(input);
+
+                if(isMatch)
+                {
+                    string[] bossArgs = input.Split('|', ':', '#');
+                    bossArgs = bossArgs.Where(x => x != "").ToArray();
+
+                    string name = bossArgs[0];
+                    string title = bossArgs[1];
+
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($"Strength: {name.Length}");
+                    Console.WriteLine($"Armour: {title.Length}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Final Exam Exercises/Warrior’s Quest/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Warrior_s_Quest
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] code = Console.ReadLine().ToCharArray();
+
+            string input = Console.ReadLine();
+    
+            while (input != "For Azeroth")
+            {
+                string[] commandArgs = input.Split();
+
+                string command = commandArgs[0];
+
+                if(command == "GladiatorStance")
+                {
+                    code = code.Select(x => char.ToUpper(x)).ToArray();
+                    Console.WriteLine(code);
+                }
+                else if(command == "DefensiveStance")
+                {
+                    code = code.Select(x => char.ToLower(x)).ToArray();
+                    Console.WriteLine(code);
+                }
+                else if(command == "Dispel")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    char letter = char.Parse(commandArgs[2]);
+
+                    if(index < 0 || index > code.Length - 1)
+                    {
+                        Console.WriteLine("Dispel too weak.");
+                    }
+                    else
+                    {
+                        code[index] = letter;
+                        Console.WriteLine("Success!");
+                    }
+                }
+                else if(command == "Target")
+                {
+                    if(commandArgs[1] == "Change")
+                    {
+                        string sub = commandArgs[2];
+                        string secondSub = commandArgs[3];
+
+                        string codeToStr = new string(code);
+
+                        codeToStr = codeToStr.Replace(sub, secondSub);
+
+                        Console.WriteLine(codeToStr);
+
+                        code = codeToStr.ToCharArray();
+
+                    }
+                    else if(commandArgs[1] == "Remove")
+                    {
+                        string sub = commandArgs[2];
+
+                        string codeToStr = new string(code);
+
+                        codeToStr = codeToStr.Replace(sub, "");
+
+                        Console.WriteLine(codeToStr);
+
+                        code = codeToStr.ToCharArray();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Command doesn't exist!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
